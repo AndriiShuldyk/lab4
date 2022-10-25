8da47407186b674a3e0ef876a33beb91212e3b08
AndriiShuldyk <112167667+AndriiShuldyk@users.noreply.github.com>
2022-10-25 21:49:31 +0300

    Add files via upload

---
+++ lab4_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+class Printer
+{
+    public virtual void Print(string argument)
+    {
+        Console.WriteLine(argument);
+        Console.ForegroundColor = ConsoleColor.Black;
+    }
+}
+
+class FirstLine : Printer
+{
+    public override void Print(string argument)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        base.Print(argument);
+    }
+}
+
+class SecondLine : Printer
+{
+    public override void Print(string argument)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        base.Print(argument);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Printer firstLine = new FirstLine();
+        firstLine.Print("first line");
+        Printer secondLine = new SecondLine();
+        secondLine.Print("second line");
+    }
+}
